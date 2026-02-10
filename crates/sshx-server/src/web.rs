0e84e2bff65e27000979e00d25a2424614cae2fd
weng-td <mamacooks0011@gmail.com>
2026-02-10 14:24:57 +0000

    Fix CORS and hardcode backend API URL for Cloudflare deployment

--- crates/sshx-server/src/web.rs
+++ crates/sshx-server/src/web.rs
@@ -5,6 +5,7 @@ use std::sync::Arc;
 use axum::routing::{any, get_service};
 use axum::Router;
 use tower_http::services::{ServeDir, ServeFile};
+use tower_http::cors::CorsLayer;
 
 use crate::ServerState;
 
@@ -32,6 +33,7 @@ pub fn app() -> Router<Arc<ServerState>> {
 /// Routes for the backend web API server.
 fn backend() -> Router<Arc<ServerState>> {
     Router::new()
+        .layer(CorsLayer::permissive())
         .merge(admin::routes())
         .route("/s/{name}", any(socket::get_session_ws))
 }
---
+++ dist/_app/immutable/chunks/navigation.536363b5.js
@@ -0,0 +1,1 @@
+import{j as o}from"./singletons.f084d2f0.js";const e=o("goto");export{e as g};
